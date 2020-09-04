1761804bcabcc92a594468c3ffb15cc1e1e7d3b9
Anatoliy Chirkov <chirkoff.ab@gmail.com>
2020-09-04 13:55:19 +0300

    fix common exception

--- src/Http/Actions/Action.php
+++ src/Http/Actions/Action.php
@@ -5,7 +5,7 @@ namespace ProductsApi\Http\Actions;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpBadRequestException;
-use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpInternalServerErrorException;
 
 abstract class Action
 {
@@ -29,7 +29,7 @@ abstract class Action
      * @param Response $response
      * @param array    $args
      * @return Response
-     * @throws HttpNotFoundException
+     * @throws HttpInternalServerErrorException
      * @throws HttpBadRequestException
      */
     public function __invoke(Request $request, Response $response, $args): Response
@@ -40,8 +40,8 @@ abstract class Action
 
         try {
             return $this->action();
-        } catch (/*DomainRecordNotFoundException*/\Exception $e) {
-            throw new HttpNotFoundException($this->request, $e->getMessage());
+        } catch (\Exception $e) {
+            throw new HttpInternalServerErrorException($this->request, $e->getMessage());
         }
     }
 
